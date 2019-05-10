809efc771f41d2ae49ff3089777d6f5c9359d357
marco5de <marco.deuscher@t-online.de>
2019-05-10 17:21:10 +0200

    added plots with labels

--- exercise01/sh01ex01.m
+++ exercise01/sh01ex01.m
@@ -1,8 +1,22 @@
 im1 = imread("images/obstA.png");
 im2 = imread("images/obstB.png");
 
-image(im2);
-H=myHistogram(im2);
 
-plot(x,H)
+H1=myHistogram(im1);
+image(im2);
+H2=myHistogram(im2);
+
+x=1:256;
+
+clear title xlabel ylabel
+subplot(2,2,1),image(im1),title("Im1")
+subplot(2,2,2),plot(x,H1),title("Histogram Im1")
+xlabel("Intensity"),ylabel("Probability")
+
+
+subplot(2,2,3),image(im2),title("Im2")
+subplot(2,2,4),plot(x,H2),title("Histogram Im2")
+xlabel("Intensity"),ylabel("Probability")
+
+
 
